53d10901d39d8f7be47dada885e76ac47e6d3273
Andrej Pekar <pekand@gmail.com>
2024-05-27 17:09:09 +0200

    create diagram plugin

--- Diagram/src/Plugins/interfaces/IDiagramPlugin.cs
+++ Diagram/src/Plugins/interfaces/IDiagramPlugin.cs
@@ -11,6 +11,7 @@
         // plugin location for resource mapping
         void SetLocation(string location);
 
+        
         // connection to program debug console
         void SetLog(Log log);
 
